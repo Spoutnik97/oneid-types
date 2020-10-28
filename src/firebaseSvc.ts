0c66224ae5669e1c59a9b5beb7f772c21a1ad843
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2020-10-28 16:11:25 +0100

    (firebaseSvc) add transaction and set

--- src/firebaseSvc.ts
+++ src/firebaseSvc.ts
@@ -1,8 +1,9 @@
 export type FirebaseSvcType = {
   getOnceAsync: (ref: string) => Promise<any>;
   getRealtime: (ref: string, callback: (data: any) => void) => any;
-  set: (ref: string, data: object) => Promise<void>;
+  set: (ref: string, data: object | string) => Promise<void>;
   off: (ref: string, realtimeRef: any) => void;
   remove: (ref: string) => void;
   resetPassword: (email: string) => Promise<void>;
+  transaction: (ref: string, fct: (counter: number) => void) => Promise<void>;
 };
