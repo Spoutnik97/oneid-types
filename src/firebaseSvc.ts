c20faba54e3d7587a35029272443a6ca160fcf29
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2020-10-30 09:30:21 +0100

    (firebaseSvc) fix types

--- src/firebaseSvc.ts
+++ src/firebaseSvc.ts
@@ -1,9 +1,9 @@
 export type FirebaseSvcType = {
   getOnceAsync: (ref: string) => Promise<any>;
   getRealtime: (ref: string, callback: (data: any) => void) => any;
-  set: (ref: string, data: object | string) => Promise<void>;
+  set: (ref: string, data: any) => Promise<void>;
   off: (ref: string, realtimeRef: any) => void;
   remove: (ref: string) => void;
   resetPassword: (email: string) => Promise<void>;
-  transaction: (ref: string, fct: (counter: number) => void) => Promise<void>;
+  transaction: (ref: string, fct: (item: any | null) => void) => Promise<void>;
 };
