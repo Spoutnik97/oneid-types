7cc3e84876ff5e46bc1af5070085372504a82032
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2020-11-16 12:54:06 +0100

    (firebaseSvc) fix createUser type

--- src/firebaseSvc.ts
+++ src/firebaseSvc.ts
@@ -12,7 +12,7 @@ export type FirebaseSvcType = {
   createUser: (user: {
     email: string;
     password: string;
-  }) => Promise<{ user: { uid: string; email: string } | null }>;
+  }) => Promise<{ user: { uid: string; email: string | null } | null }>;
   getOnceAsync: (ref: string) => Promise<any>;
   getRealtime: (ref: string, callback: (data: any) => void) => any;
   set: (ref: string, data: any) => Promise<void>;
