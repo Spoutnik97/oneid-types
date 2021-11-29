c91e384ac1773981157468f5d69d3d0a8c559a77
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2021-11-29 08:40:53 +0100

    built files

--- lib/src/event.d.ts
+++ lib/src/event.d.ts
@@ -14,15 +14,15 @@ declare type TicketConfigBase = {
     times: number;
     timeMode: "any" | "eachDay";
 };
-interface TicketConfigField extends TicketConfigBase {
+export interface TicketConfigField extends TicketConfigBase {
     type: "field";
     fieldKey: string;
     values: string[];
 }
-interface TicketConfigAll extends TicketConfigBase {
+export interface TicketConfigAll extends TicketConfigBase {
     type: "all";
 }
-interface TicketConfigAllotment extends TicketConfigBase {
+export interface TicketConfigAllotment extends TicketConfigBase {
     type: "allotment";
 }
 export declare type TicketConfig = TicketConfigField | TicketConfigAll | TicketConfigAllotment;
