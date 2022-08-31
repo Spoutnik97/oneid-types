2cc9f75f5e2435b9de3b9a57e4cabdaf736f43d3
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2022-08-31 09:35:47 +0200

    built files (hello asso)

---
+++ lib/src/helloAsso.d.ts
@@ -0,0 +1,32 @@
+export declare type RequestCheckoutBody = {
+    totalAmount: number;
+    initialAmount: number;
+    itemName: string;
+    backUrl: string;
+    errorUrl: string;
+    returnUrl: string;
+    containsDonation: boolean;
+    payer?: {
+        firstName: string;
+        lastName: string;
+        email: string;
+        dateOfBirth: string;
+        address: string;
+        city: string;
+        zipCode: string;
+        country: string;
+    };
+    metadata?: Record<string, any>;
+    trackingParameter?: string;
+};
+export declare type RefreshLoginBody = {
+    client_id: string;
+    refresh_token: string;
+    grant_type: "refresh_token";
+};
+export declare type LoginBody = {
+    client_id: string;
+    client_secret: string;
+    grant_type: "client_credentials";
+};
+//# sourceMappingURL=helloAsso.d.ts.map
